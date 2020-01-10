9ece08c38314e4b61022f6502406da2c59250c2d
matthiashugli <development@keepertrainer.ch>
2020-01-10 11:40:38 +0100

    fix static files root

--- keepertrainer/settings.py
+++ keepertrainer/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'keepertrainer.urls'
@@ -127,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'theme/static/'
 
 # Extra places for collectstatic to find static staticfiles
 STATICFILES_DIRS = (
